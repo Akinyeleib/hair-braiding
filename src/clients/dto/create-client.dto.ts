fd46d70e2b52992b841835d4f9a1bea4f5090f2a
Ibrahim Akinyele <iakinyele3@gmail.com>
2023-12-17 11:31:18 +0100

    create-client-dto populated

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,1 +1,32 @@
-export class CreateClientDto {}
+import { IsEmail, IsNotEmpty, IsNumberString, IsString } from 'class-validator';
+export class CreateClientDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+  @IsEmail()
+  email: string;
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+  @IsString()
+  @IsNotEmpty()
+  state: string;
+  @IsString()
+  @IsNotEmpty()
+  country: string;
+  @IsString()
+  @IsNotEmpty()
+  gender: string;
+  @IsString()
+  @IsNotEmpty()
+  image_url: string;
+  @IsNumberString()
+  @IsNotEmpty()
+  phone_number: string;
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+  @IsString()
+  @IsNotEmpty()
+  status: string;
+}
