b6eaf2e553aa807567d75818d25321e8d99ca84c
Ibrahim Akinyele <iakinyele3@gmail.com>
2023-12-20 21:05:38 +0100

    client, style, and stylist modules exported

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -3,10 +3,13 @@ import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Appointment } from './entities/appointment.entity';
+import { ClientsModule } from 'src/clients/clients.module';
+import { StylesModule } from 'src/styles/styles.module';
+import { StylistsModule } from 'src/stylists/stylists.module';
 
 @Module({
   controllers: [AppointmentsController],
-  imports: [TypeOrmModule.forFeature([Appointment])],
+  imports: [TypeOrmModule.forFeature([Appointment]), ClientsModule, StylesModule, StylistsModule],
   providers: [AppointmentsService],
 })
 export class AppointmentsModule {}
