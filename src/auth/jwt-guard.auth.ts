13c357af7682c8386ebadfafbd8059d0980022de
Ibrahim Akinyele <iakinyele3@gmail.com>
2023-12-21 14:53:35 +0100

    jwt auth guard implementation

---
+++ src/auth/jwt-guard.auth.ts
@@ -0,0 +1,35 @@
+import { CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { configDotenv } from "dotenv";
+import { Request } from "express";
+configDotenv();
+
+export class JWTGuard implements CanActivate {
+
+  constructor(
+    private jwtService: JwtService
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) throw new UnauthorizedException("Invalid token");
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {secret : process.env.JWT_SECRET});
+      request['user'] = payload;
+    } catch (error) {
+      throw new UnauthorizedException("Invalid token: " + error);
+    }
+
+    return false;
+  }
+
+  private extractTokenFromHeader(request: Request) : string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+
+}
