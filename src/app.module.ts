af794019f84e3f78222bacaa93e2d79b6db26788
Ibrahim Akinyele <iakinyele3@gmail.com>
2023-12-21 13:42:27 +0100

    jwt initialised

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,12 @@ import { StylesModule } from './styles/styles.module';
 import { AppointmentsModule } from './appointments/appointments.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    ClientsModule, StylistsModule, RatingsModule, StylesModule, AppointmentsModule],
+    ClientsModule, StylistsModule, RatingsModule, StylesModule, AppointmentsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
