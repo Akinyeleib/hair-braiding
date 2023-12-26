961242a2879b57cea42198efad8d991c192890d4
Ibrahim Akinyele <iakinyele3@gmail.com>
2023-12-26 19:49:40 +0100

    resend email integration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,9 @@ import { ClientsService } from 'src/clients/clients.service';
 import { LoginDTO } from './dto/login.dto';
 import { CreateClientDto } from 'src/clients/dto/create-client.dto';
 import { VerifyEmailAddressDTO } from './dto/verify-email.dto';
+import { Resend } from 'resend';
+import { configDotenv } from 'dotenv';
+configDotenv();
 
 @Injectable()
 export class AuthService {
@@ -31,10 +34,38 @@ export class AuthService {
     
     const {email} = verifyEmailAddressDTO;
 
-    const user = await this.clientService.findUserbyEmail(email);
-    if (!user) throw new NotFoundException("Invalid email address");
+    // const user = await this.clientService.findUserbyEmail(email);
+    // if (!user) throw new NotFoundException("Invalid email address");
 
-    return 'email successfully verified';
+    const resend = new Resend(process.env.EMAIL_API_KEY);
+
+    const { data, error } = await resend.emails.send({
+      from: 'iakinyele3@gmail.com',
+      to: verifyEmailAddressDTO.email,
+      subject: 'Hello World',
+      html: '<p>Congrats on sending your <strong>first email from nest-js</strong>!</p>',
+    });
+
+    if (error) return error
+    return {
+      message : "success",
+      data
+    }
+
+    /*
+    .then((data) => {
+      console.log("success");
+      console.log({data});
+      return {result: data}
+    })
+    .catch((err) => {
+      console.log("failed");
+      console.log({err});
+      return {err}
+    });
+    */
+
+    // return 'email successfully verified';
     
   }
 
